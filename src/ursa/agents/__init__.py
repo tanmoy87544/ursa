7c65bc922c700aa93e7c7c00cbee7b0eec7b7005
Mike Grosskopf <mikegros@lanl.gov>
2025-08-14 14:19:27 -0600

    Fixing two typos

--- src/ursa/agents/__init__.py
+++ src/ursa/agents/__init__.py
@@ -4,7 +4,7 @@ from .execution_agent         import    ExecutionAgent,    ExecutionState
 from .code_review_agent       import   CodeReviewAgent,   CodeReviewState
 from .hypothesizer_agent      import HypothesizerAgent, HypothesizerState
 from .arxiv_agent             import ArxivAgent, PaperState, PaperMetadata
-from .memory_agent            import RecallAgent
+from .recall_agent            import RecallAgent
 from .mp_agent                import MaterialsProjectAgent
 from .base                    import BaseAgent, BaseChatModel
 
